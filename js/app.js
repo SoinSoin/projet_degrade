086faad8e8505e9c4d540dab9e409b74a1f05442
SoinSoin <vincent.retore@gmail.com>
2018-01-29 12:52:33 +0100

    menu gauche quasi fini

--- js/app.js
+++ js/app.js
@@ -1,9 +1,42 @@
-console.log('zizi');
-$(document).ready(function() {
+
+  console.log("hamburger");
+
+var forEach = function (t, o, r) {
+    if ("[object Object]" === Object.prototype.toString.call(t))
+        for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+    else
+        for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t)
+};
+
+var hamburgers = document.querySelectorAll(".hamburger");
+if (hamburgers.length > 0) {
+    forEach(hamburgers, function (hamburger) {
+        hamburger.addEventListener("click", function () {
+            this.classList.toggle("is-active");
+        }, false);
+    });
+}
+
+var myButton = false;
+
+
  
-    // Fakes the loading setting a timeout
-      setTimeout(function() {
-          $('body').addClass('loaded');
-      }, 3500);
-   
-  }
+function clickOnButton()
+{
+    if (myButton)
+    {
+        
+        $("#menu").animate({left:'-15rem'},{ duration:400}) && $("input").prop('disabled', false) && $(".filter").hide();
+        myButton = false;
+    }
+    else
+    {
+        $("#menu").animate({left:'0rem'},{duration:400}) && $("input").prop('disabled', true) && $(".filter").show();
+        myButton = true;
+    }
+}
+
+
+ 
+$("#burger").click(function() { clickOnButton(); } );
+
