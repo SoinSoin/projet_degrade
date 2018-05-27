398b43dc5ae07a2dc3a33b5ce950e934028df790
SoinSoin <vincent.retore@gmail.com>
2018-05-27 11:56:51 +0200

    animation set up

--- src/Components/nav/Navbar.js
+++ src/Components/nav/Navbar.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import Button from './ButtonBurger';
 import Toolbar from './Toolbar';
 import './css/style.css';
 // import * as Semantic from 'semantic-ui-react';
 // import 'semantic-ui-css/semantic.css';
 
-// var wrapperMenu = document.querySelector('.wrapper-menu');
-// ReactDOM.wrapperMenu.addEventListener('click', function () {
-//   wrapperMenu.classList.toggle('open');
-// })
 export default class Navbar extends Component {
-
   render() {
     return (
       <nav >
-        <Button/>
-        <Toolbar/>
+        <Toolbar />
       </nav>
     );
   }
--- src/Components/nav/Toolbar.js
+++ src/Components/nav/Toolbar.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
 import './css/style.css';
+import Button from './ButtonBurger';
+import Anime from 'react-anime'
+
+const MyToolbar = ({ condition, handleClick }) => {
+    // console.log(condition)
+    if (condition === false)
+        return (
+            <div onClick={handleClick} className="Toolbar pulse">
+                <Button handleClick={handleClick} condition={condition} />
+                <div className="menu"/>
+            </div>
+        )
+
+
+    return (
+        <div onClick={handleClick} className="Toolbar">
+            <Button handleClick={handleClick} condition={condition} />
+            <Anime easing="easeInOutExpo"
+                duration={650}
+                translateX='15em'
+                translateY='100vh'
+                scale={[1, 1]}>
+                <div className="menu"/>
+            </Anime>
+        </div>
+    )
+
+}
 
 
 export default class Toolbar extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            condition: false
+        };
+    }
 
+    handleClick() {
+        this.setState({
+            condition: !this.state.condition
+        })
+    }
     render() {
         return (
-        <div className="Toolbar">
-        
-        </div>
-    )
+            <MyToolbar handleClick={this.handleClick} condition={this.state.condition}></MyToolbar>
+        )
+
     }
 
 }
